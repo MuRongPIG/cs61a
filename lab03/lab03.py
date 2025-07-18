975cc6906fdf1fd01ce2b225aba205dd106e15bd
Zhu S.R. <murongpig@gmail.com>
2025-07-18 15:45:06 +0800

    lab03

---
+++ lab03/lab03.py
@@ -0,0 +1,88 @@
+SOURCE_FILE = __file__
+
+
+def double_eights(n):
+    """Returns whether or not n has two digits in row that
+    are the number 8.
+
+    >>> double_eights(1288)
+    True
+    >>> double_eights(880)
+    True
+    >>> double_eights(538835)
+    True
+    >>> double_eights(284682)
+    False
+    >>> double_eights(588138)
+    True
+    >>> double_eights(78)
+    False
+    >>> # ban iteration
+    >>> from construct_check import check
+    >>> check(SOURCE_FILE, 'double_eights', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n < 88: return False
+    return True if n % 100 == 88 else double_eights(n//10)
+
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if a < b : a, b = b, a
+    return b if a % b == 0 else gcd(b, a % b)
+
+def func(i):
+    if i == 0: return 1
+    res = 1
+    while i != 1:
+        res *= i
+        i -= 1
+    return res                # new func
+
+def pascal(row, column):
+    """Returns the value of the item in Pascal's Triangle
+    whose position is specified by row and column.
+    >>> pascal(0, 0)    # The top left (the point of the triangle)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 3 (1 3 3 1), Column 2
+    3
+    >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    if column > row:return 0
+    return int(func(row) / (func(column) * func(row-column)))
+
+
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    return int(func(m+n-2) / (func(m-1) * func(n-1)))
+
