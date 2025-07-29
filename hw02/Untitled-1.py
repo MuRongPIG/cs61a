e96f1d369380a1c0dd4aec661b074168f27caaad
Zhu S.R. <murongpig@gmail.com>
2025-07-29 18:54:54 +0800

    hw02

---
+++ hw02/Untitled-1.py
@@ -0,0 +1,23 @@
+def shuffle(s):
+    """Return a shuffled list that interleaves the two halves of s.
+
+    >>> shuffle(range(6))
+    [0, 3, 1, 4, 2, 5]
+    >>> letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    >>> shuffle(letters)
+    ['a', 'e', 'b', 'f', 'c', 'g', 'd', 'h']
+    >>> shuffle(shuffle(letters))
+    ['a', 'c', 'e', 'g', 'b', 'd', 'f', 'h']
+    >>> letters  # Original list should not be modified
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    """
+    assert len(s) % 2 == 0, 'len(seq) must be even'
+    "*** YOUR CODE HERE ***"
+    a = []
+    b = list(s)
+    while b:
+        a.append(b.pop(0))
+        a.append(b.pop(len(b) // 2))
+    return a
+
+shuffle(range(6))
